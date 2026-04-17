449cab75c1eff2b8d01570b8bc7a306fd27c6dca
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2026-04-17 15:29:47 +0200

    Updated textures of skyscrapers and pod in assets map.

--- core/src/main/java/io/github/jumpyBirb/graphics/GameAssets.java
+++ core/src/main/java/io/github/jumpyBirb/graphics/GameAssets.java
@@ -38,7 +38,7 @@ import com.badlogic.gdx.graphics.Texture;
 public class GameAssets {
     public final Texture background = new Texture("background.png");
     public final Texture player = new Texture("player.png");
-    public final Texture skyscraper = new Texture("skyscraper.png");
+    public final Texture skyscraper = new Texture("retrowave_skyscrapers_bottom-01.png");
     public final Texture pod = new Texture("podPlatform.png");
     public final Texture parallax1 = new Texture("clouds-parallax1-3000x1080.png");
     public final Texture parallax2 = new Texture("city-parallax2-1920x1080.png");
